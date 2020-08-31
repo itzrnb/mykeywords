3f968fb3833cf73271eebbce48d025a8359fc003
itzrnb <itzrnb@gmail.com>
2020-09-01 00:52:06 +0900

    web server 기능 추가

--- main.go
+++ main.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
+
 	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type item struct {
-	StoryURL  string
-	Source    string
-	comments  string
-	CrawledAt time.Time
-	Comments  string
-	Title     string
-}
+// 찾은 링크 하나하나
+type (
+	Item struct {
+		Count     int    `json:"count"`
+		StoryURL  string `json:"storyurl"`
+		Source    string `json:"source"`
+		comments  string
+		CrawledAt time.Time
+		Comments  string `json:"comments"`
+		Title     string `json:"title"`
+	}
+)
 
-//var links = []string{}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintln(w, links)
-}
+// 어떤 포탈? 어떤 키워드?
+const (
+	max_item_count = 30
+)
 
 func main() {
-	stories := []item{}
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains: search.naver.com
@@ -35,63 +38,60 @@ func main() {
 		colly.Async(true),
 	)
 
-	// On every a element which has .top-matter attribute call callback
-	// This class is unique to the div that holds all information about a story
-
-	// <div class="top-matter"><p class="title"><a class="title may-blank outbound" data-event-action="title" href=".....
-	// On every a element which has .top-matter attribute call callback
-	// This class is unique to the div that holds all information about a story
-	// c.OnHTML(".top-matter", func(e *colly.HTMLElement) {
-	// 	temp := item{}
-	// 	temp.StoryURL = e.ChildAttr("a[data-event-action=title]", "href")
-	// 	temp.Source = "https://old.reddit.com/r/programming/"
-	// 	temp.Title = e.ChildText("a[data-event-action=title]")
-	// 	temp.Comments = e.ChildAttr("a[data-event-action=comments]", "href")
-	// 	temp.CrawledAt = time.Now()
-	// 	stories = append(stories, temp)
-	// })
-
-	//<table border="0" class='comment-tree'>
-	//		<tr class='athing comtr
-	//c.OnHTML(".comment-tree tr.athing", func(e *colly.HTMLElement) {
-
+	Items := []Item{}
 	count := 0
+
+	currentpagecount := 0
 	// <dt><a class="sh_blog_title _sp_each_url _sp_each_title" href="link">
 	c.OnHTML("dt a.sh_blog_title", func(e *colly.HTMLElement) {
-		temp := item{}
-		//temp.comments = e.Attr("a", "class")
-		temp.StoryURL = e.Attr("href")
-		if temp.StoryURL == "" {
+		if count == max_item_count {
 			return
 		}
-		// temp.Source = "https://search.naver.com/"
-		temp.Title = e.Text
-		// temp.Comments = e.ChildAttr("a[data-event-action=comments]", "href")
-		// temp.CrawledAt = time.Now()
+		if currentpagecount == 0 {
+			currentpagecount = 1
+		}
 
-		//temp.StoryURL = e.Attr("href")
-		// temp.Source = "https://search.naver.com/"
-		//temp.Title = e.ChildText("a[data-event-action=title]")
-		// temp.Comments = e.ChildAttr("a[data-event-action=comments]", "href")
-		// temp.CrawledAt = time.Now()
-		// matched, _ := regexp.MatchString(`https://blog.naver.com/.*`, e.Attr("href"))
-		// if matched == true {
-		// 	fmt.Println(e.Attr("href"))
-		// 	links = append(links, e.Attr("href")+"\n")
-		// }
+		item := Item{}
+		//temp.comments = e.Attr("a", "class")
+		item.StoryURL = e.Attr("href")
+		if item.StoryURL == "" {
+			return
+		}
+		item.Source = "https://search.naver.com/"
+		item.Title = e.Text
+		item.CrawledAt = time.Now()
+		item.Count = count + 1
 
-		stories = append(stories, temp)
-		count = count + 1
-		fmt.Println("Link:", count, temp.StoryURL)
-		fmt.Println("Title:", count, temp.Title)
+		Items = append(Items, item)
+		count = item.Count
 	})
 
-	// // On every span tag with the class next-button
-	//<span class="next-button">
-	// c.OnHTML("span.next-button", func(h *colly.HTMLElement) {
-	// 	t := h.ChildAttr("a", "href")
-	// 	c.Visit(t)
-	// })
+	maxpagecount := 0
+	c.OnHTML(".paging", func(e *colly.HTMLElement) {
+		if maxpagecount != 0 {
+			return
+		}
+		if pagecount := len(e.ChildAttrs("a", "href")); pagecount != 0 {
+			maxpagecount = max_item_count / pagecount
+		}
+	})
+
+	c.OnHTML(".next", func(e *colly.HTMLElement) {
+		if maxpagecount == 0 {
+			return
+		}
+
+		if currentpagecount == maxpagecount {
+			return
+		}
+
+		currentpagecount = currentpagecount + 1
+		c.Visit("https://search.naver.com/search.naver" + e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
 	// Set max Parallelism and introduce a Random Delay
 	c.Limit(&colly.LimitRule{
@@ -99,34 +99,52 @@ func main() {
 		RandomDelay: 5 * time.Second,
 	})
 
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/test", func(context echo.Context) error {
+		// 재검색을 대비한 주요 객체 초기화
+		defer func() {
+			Items = nil
+			count = 0
+			maxpagecount = 0
+			currentpagecount = 0
+			c.Init()
+		}()
+
+		m := echo.Map{}
+		if err := context.Bind(&m); err != nil {
+			return err
+		}
+
+		// 일단은 네이버 블로그만
+		// 차후 검색 가능 portal list 별도로 분리
+		if m["portal"] != "naverblog" {
+			return context.JSON(http.StatusBadRequest, m)
+		}
+
+		// 검색할 키워드 체크
+		if m["keyword"] == "" {
+			return context.JSON(http.StatusBadRequest, m)
+		}
+
+		// 아래의 Sprintf 사용하는 방식 대신 m["keyword"].(string) 이렇게 type assertion 사용하는 방식으로 사용함
+		//Keyword := fmt.Sprintf("%v", m["keyword"])
+
+		// 복수 키워드 가능 구현할 때 주석 해제
+		//for _, keyword := range keywords {
+		addressNaverBlog := "https://search.naver.com/search.naver?nso=so:dd,a:t,p:1w&srchby=title&st=date&where=post&query=" + m["keyword"].(string)
+		c.Visit(addressNaverBlog)
+		//}
+
+		c.Wait()
+
+		return context.JSON(http.StatusOK, Items)
 	})
 
-	if len(os.Args) < 2 {
-		log.Println("검색할 키워드를 입력해 주세요.")
-		os.Exit(1)
-	}
-
-	// Crawl all reddits the user passes in
-	//keywords := []string{"https://search.naver.com/search.naver?where=post&sm=tab_jum&query=붕붕카", "https://search.naver.com/search.naver?where=post&sm=tab_jum&query=밥상"}
-	keywords := os.Args[1:]
-	for _, keyword := range keywords {
-		addressNaverBlog := "https://search.naver.com/search.naver?nso=so:dd,a:t,p:1w&srchby=title&st=date&where=post&query=" + keyword
-		c.Visit(addressNaverBlog)
-		fmt.Println(addressNaverBlog)
-	}
-
-	// 	fmt.Println(addressNaverBlog)
-
-	c.Wait()
-	//fmt.Println(stories)
-	//fmt.Println(links)
-
-	http.HandleFunc("/", handler)
-	// 일단 아래 웹서버 비활성화
-	//log.Fatal(http.ListenAndServe(":7777", nil))
+	e.Logger.Fatal(e.Start(":3000"))
 }
 
 // // 붕붕카, 최신순, 1주일, 제목만 체크
@@ -138,13 +156,3 @@ func main() {
 
 // // 아래가 최소 검색 페이지 까지 생략
 // https://search.naver.com/search.naver?nso=so:dd,a:t,p:1w&srchby=title&st=date&where=post&query=붕붕카
-
-// c.OnHTML(".top-matter", func(e *colly.HTMLElement) {
-// 		temp := item{}
-// 		temp.StoryURL = e.ChildAttr("a[data-event-action=title]", "href")
-// 		temp.Source = "https://old.reddit.com/r/programming/"
-// 		temp.Title = e.ChildText("a[data-event-action=title]")
-// 		temp.Comments = e.ChildAttr("a[data-event-action=comments]", "href")
-// 		temp.CrawledAt = time.Now()
-// 		stories = append(stories, temp)
-// 	})
